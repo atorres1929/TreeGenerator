bea7155dfddd2b2d68e632388ca9b66ee6a9895f
Adam Torres <atorres1929@gmail.com>
2018-12-19 15:22:46 -0500

    Set the basis for constructing and drawing the tree

--- TreeGenerator.py
+++ TreeGenerator.py
@@ -1,11 +1,12 @@
 import tkinter
 import random
+
 from queue import Queue
 
 
 class TreeGenerator:
 
-    def __init__(self, tree):
+    def __init__(self):
         self.root = tkinter.Tk(screenName="Tree Generator")
         self.is_fullscreen = True
         self.root.attributes("-fullscreen", self.is_fullscreen)
@@ -14,16 +15,16 @@ class TreeGenerator:
         config = {'width': self.WIDTH, 'height': self.HEIGHT}
         self.canvas = tkinter.Canvas(self.root, config)
         self.canvas.pack()
-        self.root.bind("<Escape>", self.toggle_fullscreen)
-        self.tree = tree
+        self.root.bind("<Escape>", self.__toggle_fullscreen)
+        self.tree = None
 
-    def toggle_fullscreen(self, event=None):
-        self.is_fullscreen = not self.is_fullscreen
-        self.root.attributes("-fullscreen", self.is_fullscreen)
-        return "break"
+    def create_tree_middle_top(self, weight):
+        self.tree = Tree(weight, self.WIDTH / 2, 0)
 
     def generate(self):
-        self.tree.add_node(Node(self.tree.weight))
+        current_node = self.tree.current_node
+        self.tree.add_node(Node(current_node.CHILD_LIMIT, current_node.x+10, current_node.y+10))
+        self.tree.add_node(Node(current_node.CHILD_LIMIT, current_node.x-10, current_node.y+10))
         self.draw()
 
     def draw(self):
@@ -31,47 +32,47 @@ class TreeGenerator:
 
         queue.put(self.tree.root)
 
-        mid_width = self.WIDTH/2
-
+        delay = 500
         while queue.qsize() != 0:
 
             node = queue.get()
 
             for child in node.children:
-                self.canvas.create_line(mid_width, 0, mid_width+10, 10, width=2)
+                self.canvas.after(delay, self.canvas.create_line, node.x, node.y, child.x, child.y, {'width': 2})
+                delay += 500
+
+    def __toggle_fullscreen(self, event=None):
+        self.is_fullscreen = not self.is_fullscreen
+        self.root.attributes("-fullscreen", self.is_fullscreen)
+        return "break"
 
 
 class Tree:
 
-    def __init__(self, weight):
-        self.root = Node(weight)
+    def __init__(self, child_limit, x, y):
+        self.root = Node(child_limit, x, y)
         self.current_node = self.root
-        self.weight = weight
-        self.root.CHILD_LIMIT = random.randint(1, self.weight)
+        self.root.CHILD_LIMIT = child_limit
 
     def add_node(self, node):
-        if not len(self.current_node.children) < self.weight:
+        if not len(self.current_node.children) < self.current_node.CHILD_LIMIT:
             self.current_node = node
         self.current_node.add_child(node)
-        self.current_node = node
 
 
 class Node:
 
-    def __init__(self, weight):
+    def __init__(self, child_limit, x, y):
         self.children = []
-        self.CHILD_LIMIT = weight
-        self.length = weight * random.randint(1, weight)
+        self.CHILD_LIMIT = child_limit
+        self.x = x
+        self.y = y
 
     def get_num_children(self):
         return len(self.children)
 
     def add_child(self, node):
-        if len(self.children) < self.CHILD_LIMIT:
-            self.children.append(node)
-            return True
-        else:
-            return False
+        self.children.append(node)
 
     def remove_child_node(self, node):
         self.children.remove(node)
@@ -81,6 +82,7 @@ class Node:
 
 
 if __name__ == '__main__':
-    form = TreeGenerator(Tree(1))
+    form = TreeGenerator()
+    form.create_tree_middle_top(3)
     form.generate()
     form.root.mainloop()
